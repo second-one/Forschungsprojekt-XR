74d0660ceb1216ef5a9d3cdeedc25c95fdd47ae2
agebauer16558 <a.gebauer16558@hs-ansbach.de>
2022-06-02 18:40:31 +0200

    added new scripts
    funktionieren noch nicht

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FoodManager : MonoBehaviour
 {
@@ -8,19 +10,38 @@ public class FoodManager : MonoBehaviour
 
     public Food existingFood;
 
+    //
+    public GameObject rightHandModel;
+    public ActionBasedController controllerManager;
+    private SwitchToCutlery changeModel;
+    //
+    
+  //  private void Start()
+  //  {
+  //      controllerManager = rightHandModel.GetComponent<ActionBasedController>();
+   // }
+
     public void SpawnFoodSource (int index) 
     {
         DestroyFood();
 
         var foodCopy = Instantiate(foodToSpawn[index], new Vector3(transform.position.x, 2.2f, transform.position.z), transform.rotation);
         existingFood = foodCopy.AddComponent<Food>();
-        foodCopy.tag = "Copy";
-        foreach (Transform t in foodCopy.transform)
-        {
-            t.gameObject.tag = "Copy";
-        }
 
+        //
+        controllerManager.modelPrefab = changeModel.controllerModel;
+        print(controllerManager.modelPrefab);
+        //
     }
+        
+        
+      // foodCopy.tag = "Copy";
+      // foreach (Transform t in foodCopy.transform)
+      // {
+      //   t.gameObject.tag = "Copy";
+      // }
+
+    
 
     public void DestroyFood() 
     {
@@ -28,6 +49,7 @@ public class FoodManager : MonoBehaviour
         {
             Destroy(existingFood.gameObject);
             //Destroy(GameObject.FindWithTag("Copy"));
+
         }
     }
 
