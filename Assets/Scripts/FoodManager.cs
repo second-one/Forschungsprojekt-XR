6a45f1eb540be66f4881440642baebb7efed8929
agebauer16558 <a.gebauer16558@hs-ansbach.de>
2022-06-05 18:51:36 +0200

    added cutlery to objects, repositioned objects when grabbed, fixed rotation

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -7,50 +7,68 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class FoodManager : MonoBehaviour
 {
     public GameObject[] foodToSpawn;
-
+    public GameObject[] cutleryModels;
+    public GameObject cutleryPosition;
     public Food existingFood;
 
-    //
-    public GameObject rightHandModel;
+    private GameObject cutleryCopy;
+   /* public CutleryManager cutleryObject;*/
+
+/*  public GameObject rightHandModel;
     public ActionBasedController controllerManager;
-    private SwitchToCutlery changeModel;
-    //
-    
-  //  private void Start()
-  //  {
-  //      controllerManager = rightHandModel.GetComponent<ActionBasedController>();
-   // }
+    private SwitchToCutlery changeModel; */
+
+/*  private void Start() { controllerManager = rightHandModel.GetComponent<ActionBasedController>(); } */
+
 
     public void SpawnFoodSource (int index) 
     {
+        
         DestroyFood();
+        /*cutleryObject.DestroyCutlery();*/
+        
+        var foodCopy = Instantiate(foodToSpawn[index], new Vector3(
+            transform.position.x, 
+            2.2f, 
+            transform.position.z),
+            transform.rotation);
 
-        var foodCopy = Instantiate(foodToSpawn[index], new Vector3(transform.position.x, 2.2f, transform.position.z), transform.rotation);
+        // If the x-rotation of the prefab is not 0, reset the rotation to its original values
+        // prevents falsly imported 3d-objects from spawning sideways
+        if (foodToSpawn[index].transform.eulerAngles.x != 0 || 
+            foodToSpawn[index].gameObject.CompareTag("FoodCutlery"))
+        {
+            foodCopy.transform.eulerAngles = foodToSpawn[index].transform.eulerAngles;
+
+            cutleryCopy = Instantiate(cutleryModels[index], new Vector3(
+                cutleryPosition.transform.position.x,
+                cutleryPosition.transform.position.y,
+                cutleryPosition.transform.position.z),
+                cutleryPosition.transform.rotation);
+        }
+
         existingFood = foodCopy.AddComponent<Food>();
-
-        //
-        controllerManager.modelPrefab = changeModel.controllerModel;
-        print(controllerManager.modelPrefab);
-        //
+       
+        
+        
+/*      controllerManager.modelPrefab = changeModel.controllerModel;
+        print(controllerManager.modelPrefab);*/
+        
     }
-        
-        
-      // foodCopy.tag = "Copy";
-      // foreach (Transform t in foodCopy.transform)
-      // {
-      //   t.gameObject.tag = "Copy";
-      // }
-
-    
+  
+/*    foodCopy.tag = "Copy";
+      foreach (Transform t in foodCopy.transform)
+      {
+      t.gameObject.tag = "Copy";
+      }*/  
 
     public void DestroyFood() 
     {
         if (existingFood != null)
         {
             Destroy(existingFood.gameObject);
-            //Destroy(GameObject.FindWithTag("Copy"));
+            Destroy(cutleryCopy);
 
         }
     }
-
 }
